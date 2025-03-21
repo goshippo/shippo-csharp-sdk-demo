eb2c036174dc7454c4685f4cf0064103b577899e
Andy Hopper <Andy.Hopper@shippo.com>
2025-03-21 16:21:22 -0400

    Initial commit of sample code.

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using Shippo;
+using Shippo.Models.Components;
+using Shippo.Models.Requests;
+
+var shippo = new ShippoSDK("YOUR API KEY HERE");
+var shipment = await shippo.Shipments.CreateAsync(new ShipmentCreateRequest{
+  AddressFrom = AddressFrom.CreateAddressCreateRequest(new AddressCreateRequest
+  {
+      Name = "Shawn Ippotle",
+      Street1 = "215 Clayton St.",
+      City = "San Francisco",
+      State = "CA",
+      Zip = "94117",
+      Country = "US",
+      Phone = "+1 555 341 9393",
+      Email = "test@gmail.com"
+  }),
+  AddressTo = AddressTo.CreateAddressCreateRequest(new AddressCreateRequest
+  {
+      Name = "Mr Hippo",
+      Street1 = "1092 Indian Summer Ct",
+      City = "San Jose",
+      State = "CA",
+      Zip = "95122",
+      Country = "US",
+      Phone = "+1 555 341 9393",
+      Email = "test@gmail.com"
+  }),
+  Parcels = new List<Shippo.Models.Components.Parcels>{
+    Shippo.Models.Components.Parcels.CreateParcelCreateRequest(new ParcelCreateRequest {
+      MassUnit = WeightUnitEnum.Kg,
+      Weight = "0.2",
+      DistanceUnit = DistanceUnitEnum.Cm,
+      Height = "15.0",
+      Width = "15.0",
+      Length = "15.0"
+    })
+  }
+});
+Console.WriteLine($"Shipment {shipment.ObjectId} created.");
+var rates = await shippo.Rates.ListShipmentRatesAsync(new Shippo.Models.Requests.ListShipmentRatesRequest
+  {
+    ShipmentId = shipment.ObjectId,
+    Page = 1,
+    Results = 10
+  });
+
+foreach (var rate in rates.Results) {
+  Console.WriteLine($"{rate.Provider} {rate.Servicelevel.Name}, Arrives in {rate.EstimatedDays} days: {rate.AmountLocal}{rate.CurrencyLocal}");
+}
+var selectedRate = rates.Results.First(r => Convert.ToDouble(r.AmountLocal) < 5);
+var transaction = await shippo.Transactions.CreateAsync(
+  new CreateTransactionRequestBody(CreateTransactionRequestBodyType.TransactionCreateRequest)
+  {
+    TransactionCreateRequest = new TransactionCreateRequest
+    {
+      Rate = selectedRate.ObjectId,
+      LabelFileType = LabelFileTypeEnum.PdfA4
+    }
+});
+Console.WriteLine($"Transaction {transaction.ObjectId} created.");
+
+if (transaction.Status == TransactionStatusEnum.Queued) {
+  Console.WriteLine("Waiting for transaction to complete...");
+  while (transaction.Status == TransactionStatusEnum.Queued) {
+    transaction = await shippo.Transactions.GetAsync(transaction.ObjectId);
+  }
+}
+
+if (transaction.Status == TransactionStatusEnum.Success)
+{
+  var fileName = $"label_{transaction.ObjectId}.pdf";
+  Console.WriteLine($"Downloading label to {fileName}");
+  var labelStream = await new HttpClient().GetStreamAsync(transaction.LabelUrl);
+  var downloadsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
+  using (var fileStream = File.Create(Path.Combine(downloadsPath, fileName)))
+  {
+    await labelStream.CopyToAsync(fileStream);
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# shippo-csharp-sdk-demo
-A sample project that demonstrates how to use the Shippo C# SDK
+# Shippo C# SDK Demo
+
+
+
+This is a sample project that demonstrates how to use the Shippo C# SDK.
+
+This sample illustrates how to
+* Create a Shipment object, including sender/recipient addresses and parcel details
+* Collect shipping rates offered by the carriers configured in your Shippo account
+* Use a selected rate to request a label
+* Download the label for use in shipping your package
+
+## Running the code
+
+Before you run this code, you will need to have performed the below steps:
+1. Install a recent version of [the .NET SDK](https://dotnet.microsoft.com/en-us/download) - this sample is using .NET 8
+2. [Create a Shippo Account](https://apps.goshippo.com/join)
+3. [Generate a Shippo API Token](https://support.goshippo.com/hc/en-us/articles/360026412791-Managing-Your-API-Tokens-in-Shippo#:~:text=Generate%20a%20Token,-To%20generate%20a&text=To%20generate%20a%20Test%20Token,and%20purchase%20test%20shipping%20labels.). Since this is a sample app, it is recommended that you generate a test token rather than a production (i.e., paid) token.
+
